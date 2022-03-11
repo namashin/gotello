6b1ad814cc3e55a05461bbfbc2223aefdbd9cf5b
namashin <65502681+namashin@users.noreply.github.com>
2022-03-11 17:19:52 +0900

    Update logging.go

--- utils/logging.go
+++ utils/logging.go
@@ -8,13 +8,16 @@ import (
 
 // LoggingSettings is for saving log in specific logFile and Terminal.
 func LoggingSettings(logFile string) {
+	
+	// ファイルの読み込み。書き込み両方 | ファイルが無ければ作成 | 追記モード でlogfile作成
 	logfile, err := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalf("file=logFile err=%s", err.Error())
 	}
+	
 	multiLogFile := io.MultiWriter(os.Stdout, logfile)
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 	
-	// 標準ロガーの出力先をmultiLogFilwに設定。
+	// 標準ロガーの出力先をmultiLogFilw（os.Stdoutとlogfile）に設定。
 	log.SetOutput(multiLogFile)
 }
